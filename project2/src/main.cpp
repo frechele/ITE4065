a8778e50d3f3040ee44f9b097929151c717c68fe
Junyeong Park <jyp10987@gmail.com>
2022-10-10 12:07:22 +0900

    feat(project2): implement performance checker

--- project2/src/main.cpp
+++ project2/src/main.cpp
@@ -1,6 +1,39 @@
+#include <cstdint>
 #include <iostream>
+#include <string>
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+std::uint64_t run(int numWorkers);
 
 int main(int argc, char** argv)
 {
-    
+    if (argc != 2)
+    {
+        cerr << "usage: " << argv[0] << " <thread count>" << endl;
+        return 1;
+    }
+
+    int numWorkers = 2;
+    try
+    {
+        numWorkers = std::stoi(argv[1]);
+    }
+    catch(const std::exception& e)
+    {
+        cerr << "error: " << e.what() << endl;
+        return 2;
+    }
+
+    if (numWorkers <= 0)
+    {
+        cerr << "error: invalid thread count" << endl;
+        return 3;
+    } 
+
+    const auto count = run(numWorkers);
+
+    cout << "total: " << count << endl;
 }
---
+++ project2/src/run.cpp
@@ -0,0 +1,81 @@
+#include "wfa.hpp"
+
+#include <cstdint>
+#include <chrono>
+#include <condition_variable>
+#include <vector>
+#include <thread>
+#include <memory>
+#include <random>
+#include <mutex>
+
+#include <iostream>
+
+std::mutex mut;
+std::condition_variable cv;
+bool flag;
+int readyCounter;
+
+std::vector<std::uint64_t> counter;
+std::random_device rd;
+
+void worker(int rank, WFASnapshot& wfa)
+{
+    std::mt19937 engine(rd());
+
+    {
+        std::unique_lock lock(mut);
+        --readyCounter;
+        cv.wait(lock, [] { return flag; });
+    }
+
+    while (flag)
+    {
+        const std::int64_t value = engine();
+        wfa.Update(rank, value);
+
+        ++counter[rank];
+    }
+}
+
+std::uint64_t run(int numWorkers)
+{
+    WFASnapshot wfa(numWorkers, 0);
+
+    counter.resize(numWorkers);
+    readyCounter = numWorkers;
+
+    std::vector<std::thread> workers(numWorkers);
+    for (int rank = 0; rank < numWorkers; ++rank)
+    {
+        counter[rank] = 0;
+        workers[rank] = std::thread(worker, rank, std::ref(wfa));
+    }
+
+    while (true)
+    {
+        std::scoped_lock lock(mut);
+        if (readyCounter == 0)
+        {
+            flag = true;
+            cv.notify_all();
+            break;
+        }
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(60));
+
+    flag = false;
+
+    std::uint64_t total = 0;
+    for (const auto v : counter)
+        total += v;
+
+    for (auto& worker : workers)
+    {
+        if (worker.joinable())
+            worker.join();
+    }
+
+    return total;
+}
